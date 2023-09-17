ebbafabde28d1c8fba46cc4f693d0e79a10af160
chen wind <2040690180@qq.com>
2023-09-17 22:33:44 +0800

    feat:引入vant并进行移动端适配

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,9 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+//引入vant
+import 'vant/lib/index.css'
+
 const app = createApp(App)
 
 app.use(createPinia())
