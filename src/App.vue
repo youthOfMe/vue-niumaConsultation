be00c18057bcbe2d1c21bc34c36dad46013f7f96
chen wind <2040690180@qq.com>
2023-09-17 21:16:01 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-niuma = 666
+    import { RouterLink, RouterView } from 'vue-router'
+    import HelloWorld from './components/HelloWorld.vue'
+    niuma = 666
 </script>
 
 <template>
