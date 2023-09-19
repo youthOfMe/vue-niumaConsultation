9ad4b2f3377226bced4f07613dfe76cbaa257491
youthOfMe <204069080@qq.com>
2023-09-19 09:45:55 +0800

    feat : 定义主题风格配置

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,16 @@ import { Button as VanButton } from 'vant'
 </script>
 
 <template>
-  <van-button>按钮</van-button>
+  <van-button type="primary">按钮</van-button>
+  <div class="main">main</div>
+  <div class="footer">footer</div>
 </template>
 
-<style scoped></style>
+<style scoped lang="scss">
+.main {
+  color: var(--main-color);
+}
+.footer {
+  color: var(--main-color);
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,13 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
 
-//引入vant
+// 引入vant
 import 'vant/lib/index.css'
+// 引入scss配置样式
+import '@/styles/main.scss'
 
 const app = createApp(App)
 
