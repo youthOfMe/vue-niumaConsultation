a759b7eef1e22e4c5901532750fb9f34a9f29e3a
youthOfMe <204069080@qq.com>
2023-09-21 18:14:16 +0800

    feat : 进行表单校验

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,15 +1,129 @@
-<template>
-    <h1>这是登陆页面</h1>
-    <van-button type="primary" @click="fn">牛马</van-button>
-</template>
-
 <script setup lang="ts">
-import { useRouter } from 'vue-router'
+import { ref } from 'vue'
+import { mobileRules, passwordRules } from '@/utils/rules'
+import { showToast } from 'vant'
 
-let router = useRouter()
-const fn = () => {
-    router.push('/')
+const mobile = ref('')
+const password = ref('')
+const agree = ref(false)
+let onSubmit = () => {
+    if (!agree.value) {
+        return showToast('请勾选按协议')
+    }
 }
 </script>
 
-<style scoped></style>
+<template>
+    <div class="login-page">
+        <cp-nav-bar
+            right-text="注册"
+            @click-right="$router.push('/register')"
+        ></cp-nav-bar>
+        <!-- 头部 -->
+        <div class="login-head">
+            <h3>密码登录</h3>
+            <a href="javascript:;">
+                <span>短信验证码登录</span>
+                <van-icon name="arrow"></van-icon>
+            </a>
+        </div>
+        <!-- 表单 -->
+        <van-form autocomplete="off" @submit="onSubmit()">
+            <van-field
+                placeholder="请输入手机号"
+                type="tel"
+                v-model="mobile"
+                :rules="mobileRules"
+            >
+            </van-field>
+            <van-field
+                placeholder="请输入密码"
+                type="password"
+                v-model="password"
+                :rules="passwordRules"
+            >
+            </van-field>
+            <div class="cp-cell">
+                <van-checkbox v-model="agree">
+                    <span>我已同意</span>
+                    <a href="javascript:;">用户协议</a>
+                    <span>及</span>
+                    <a href="javascript:;">隐私条款</a>
+                </van-checkbox>
+            </div>
+            <div class="cp-cell">
+                <van-button block round type="primary" native-type="submit"
+                    >登 录</van-button
+                >
+            </div>
+            <div class="cp-cell">
+                <a href="javascript:;">忘记密码？</a>
+            </div>
+        </van-form>
+        <!-- 底部 -->
+        <div class="login-other">
+            <van-divider>第三方登录</van-divider>
+            <div class="icon">
+                <img src="@/assets/qq.svg" alt="" />
+            </div>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.login {
+    &-page {
+        padding-top: 46px;
+    }
+    &-head {
+        display: flex;
+        padding: 30px 30px 50px;
+        justify-content: space-between;
+        align-items: flex-end;
+        line-height: 1;
+        h3 {
+            font-weight: normal;
+            font-size: 24px;
+        }
+        a {
+            font-size: 15px;
+        }
+    }
+    &-other {
+        margin-top: 60px;
+        padding: 0 30px;
+        .icon {
+            display: flex;
+            justify-content: center;
+            img {
+                width: 36px;
+                height: 36px;
+                padding: 4px;
+            }
+        }
+    }
+}
+.van-form {
+    padding: 0 14px;
+    .cp-cell {
+        height: 52px;
+        line-height: 24px;
+        padding: 14px 16px;
+        box-sizing: border-box;
+        display: flex;
+        align-items: center;
+        .van-checkbox {
+            a {
+                color: var(--cp-primary);
+                padding: 0 5px;
+            }
+        }
+    }
+    .btn-send {
+        color: var(--cp-primary);
+        &.active {
+            color: rgba(22, 194, 163, 0.5);
+        }
+    }
+}
+</style>
