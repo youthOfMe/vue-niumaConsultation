2a979d41c7a5033fae7674e49c04dc02c9347876
youthOfMe <204069080@qq.com>
2023-09-25 13:06:29 +0800

    feat: 首页模块-关注医生-实现适配-利用VueUse

--- src/views/Home/components/FollowDoctor.vue
+++ src/views/Home/components/FollowDoctor.vue
@@ -1,5 +1,24 @@
 <script setup lang="ts">
 import DoctorCard from './DoctorCard.vue'
+
+// 使用use
+import { useWindowSize } from '@vueuse/core'
+const { width } = useWindowSize()
+
+// 手写适配各种设备生成固定宽度
+// 组件初始化获取设备宽度，页面尺寸发生改变时获取设备宽度
+// import { ref, onMounted, onUnmounted } from 'vue'
+// const width = ref(0)
+// const setWidth = () => width.value = window.innerWidth
+// onMounted(() => {
+//     setWidth()
+//     // 进行时间监听 监听resize 屏幕尺寸是否变化 当resize背触发后进行使用事件继续监听
+//     window.addEventListener('resize',setWidth)
+// })
+// // 删除进行监听的事件
+// onUnmounted(() => {
+//     window.removeEventListener('resize',setWidth)
+// })
 </script>
 
 <template>
@@ -12,7 +31,11 @@ import DoctorCard from './DoctorCard.vue'
         </div>
         <div class="body">
             <!-- swipe 组件 -->
-            <van-swipe :width="150" :show-indicators="false" :loop="false">
+            <van-swipe
+                :width="(150 / 375) * width"
+                :show-indicators="false"
+                :loop="false"
+            >
                 <van-swipe-item v-for="item in 5" :key="item">
                     <doctor-card></doctor-card>
                 </van-swipe-item>
