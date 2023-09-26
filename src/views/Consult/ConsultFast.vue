656719dd7282dd2ada3fdfc6e6415e69e1ecc543
youthOfMe <204069080@qq.com>
2023-09-26 09:43:52 +0800

    feat: 修正极速问诊-选择问诊级别

--- src/views/Consult/ConsultFast.vue
+++ src/views/Consult/ConsultFast.vue
@@ -7,7 +7,7 @@ const consultStore = useConsultStore()
 
 <template>
     <div class="consult-fast-page">
-        <cp-nav-bar title="极速问诊" right-text="问诊记录"></cp-nav-bar>
+        <cp-native-bar title="极速问诊" right-text="问诊记录"></cp-native-bar>
         <div class="fast-logo">
             <img class="img" src="@/assets/consult-fast.png" alt="" />
             <p class="text"><span>20s</span> 快速匹配专业医生</p>
